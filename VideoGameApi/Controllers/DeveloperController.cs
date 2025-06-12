badebfb23aa7442bf37f29ec0bb64b18a2103377
Russel Aniel <anielrussel13@gmail.com>
2025-06-12 11:13:57 +0800

    Added Developer and Publisher Controller

---
+++ VideoGameApi/Controllers/DeveloperController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Data;
+using VideoGameApi.Models;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeveloperController(VideoGameDbContext context) : ControllerBase
+    {
+        protected readonly VideoGameDbContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<Developer>> GetDevelopers()
+        {
+            var developers = await _context.Developers.ToListAsync();
+
+            return Ok(developers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Developer>> GetDeveloperById(int id)
+        {
+            var developer = await _context.Developers.FindAsync(id);
+            if (developer is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(developer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Developer>> AddDeveloper(Developer newDeveloper)
+        {
+            if (newDeveloper is null)
+            {
+                return BadRequest("Developer cannot be null.");
+            }
+
+            _context.Developers.Add(newDeveloper);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetDeveloperById), new { id = newDeveloper.Id }, newDeveloper);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Developer>> UpdateDeveloper(int id, Developer updatedDeveloper)
+        {
+            var existingDeveloper = await _context.Developers.FindAsync(id);
+            if (existingDeveloper is null)
+            {
+                return NotFound();
+            }
+
+            existingDeveloper.Name = updatedDeveloper.Name;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(updatedDeveloper);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Developer>> DeleteDeveloper(int id)
+        {
+            var existingDeveloper = await _context.Developers.FindAsync(id);
+            if (existingDeveloper is null)
+            {
+                return NotFound();
+            }
+
+            _context.Developers.Remove(existingDeveloper);
+            await _context.SaveChangesAsync();
+
+            return Ok(existingDeveloper);
+        }
+    }
+}
---
+++ VideoGameApi/Controllers/PublisherController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VideoGameApi.Data;
+using VideoGameApi.Models;
+
+namespace VideoGameApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PublisherController(VideoGameDbContext context) : ControllerBase
+    {
+        protected readonly VideoGameDbContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<Publisher>> GetPublishers()
+        {
+            var publishers = await _context.Publishers.ToListAsync();
+
+            return Ok(publishers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Publisher>> GetPublisherById(int id)
+        {
+            var publisher = await _context.Publishers.FindAsync(id);
+            if (publisher is null)
+            {
+                return NotFound();
+            }
+            return Ok(publisher);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Publisher>> AddPublisher(Publisher newPublisher)
+        {
+            if(newPublisher is null)
+            {
+                return BadRequest("Publisher cannot be null.");
+            }
+
+            _context.Publishers.Add(newPublisher);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetPublisherById), new { id = newPublisher.Id }, newPublisher);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Publisher>> UpdatePublisher(int id, Publisher updatedPublisher)
+        {
+            var existingPublisher = await _context.Publishers.FindAsync(id);
+            if (existingPublisher is null)
+            {
+                return NotFound();
+            }
+
+            existingPublisher.Name = updatedPublisher.Name;
+            await _context.SaveChangesAsync();
+
+            return Ok(updatedPublisher);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Publisher>> DeletePublisher(int id)
+        {
+            var existingPublisher = await _context.Publishers.FindAsync(id);
+            if (existingPublisher is null)
+            {
+                return NotFound();
+            }
+
+            _context.Publishers.Remove(existingPublisher);
+            await _context.SaveChangesAsync();
+
+            return Ok(existingPublisher);
+        }
+    }
+}
